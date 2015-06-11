fcfd065165c0e2aa2df9ebc19606eb6a71e19430
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-06-11 15:08:41 -0400

    fixed code, added more data to csv

--- pandas_samples/res.py
+++ pandas_samples/res.py
@@ -21,10 +21,14 @@ full_df[1] = full_df[1].map(lambda x: x.lstrip(')').rstrip(')'))
 full_df.rename(columns={1:'year', 0:'res'}, inplace=True)
 # make years integers
 full_df.year = full_df.year.map(lambda x: int(x))
-# to be able to sort by them
-full_df.sort('year')
 
+# to be able to sort by them
+full_df.groupby('year').count()
 # get list of topic counts by year
 full_df.groupby(['year', 'topic']).count()
 # get list of topics
 full_df.groupby('topic').count()
+
+''' Note: earlier ASCII encoding errors were due to
+improper reading of special characters.
+'''  
