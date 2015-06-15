e1aaf195cc073eb099cad3c9fe8a922161aef34e
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-06-15 16:20:16 -0400

    full UN data, working pandas script

--- pandas_samples/res.py
+++ pandas_samples/res.py
@@ -1,31 +1,39 @@
+# coding=utf-8
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
 import pandas as pd
 
 # set column headers
 res1_cols = ['id', 'res', 'topic']
 # read in csv file of UN Security Council resolutions & topics 
 # separate by commas, set names
-res1 = pd.read_table('res.csv', sep = ',', names = res1_cols)
+res1 = pd.read_table('res3.csv', sep = ',', names = res1_cols, header=None)
 
 # split initial "res" column by " (", assign to new dataframe
 year_df = res1['res'].apply(lambda x: pd.Series(x.split(' (')))
+
 # in the new dataframe, create an index column with the
 # same name as the index column in the first dataframe
 year_df['id'] = year_df.index
 
 # merge the two dataframes - 1) topics & 2) res numbers + years
 full_df = pd.merge(res1, year_df)	
-# in the column with years, get rid of the closing parentheses	
-full_df[1] = full_df[1].map(lambda x: x.lstrip(')').rstrip(')'))
 
 # rename columns
 full_df.rename(columns={1:'year', 0:'res'}, inplace=True)
-# make years integers
-full_df.year = full_df.year.map(lambda x: int(x))
+
+# in the column with years, get rid of the closing parentheses	
+full_df['year'] = full_df['year'].map(lambda x: str(x)[:-1])
 
 # to be able to sort by them
 full_df.groupby('year').count()
+
 # get list of topic counts by year
 full_df.groupby(['year', 'topic']).count()
+
 # get list of topics
 full_df.groupby('topic').count()
 
