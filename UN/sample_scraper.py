ea2b4c6d71ee8c71bd118be5ee0815a0a5757892
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-06-23 00:27:56 -0400

    updated UN project question, added sample scraper

---
+++ UN/sample_scraper.py
@@ -0,0 +1,31 @@
+# working on a script that would loop through all years btween 1994-2015
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import csv
+import requests
+
+from bs4 import BeautifulSoup
+
+url = 'http://www.un.org/en/sc/meetings/records/2015.shtml'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+
+table = soup.find('table', attrs={'class': 'tablefont'})
+
+list_of_rows = []
+for row in table.findAll('tr'):
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        text = cell.text.replace('&nbsp;', '')
+        list_of_cells.append(text)
+    list_of_rows.append(list_of_cells)
+
+outfile = open("./cells15.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerows(list_of_rows)
