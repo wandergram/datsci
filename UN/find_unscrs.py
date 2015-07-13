99e90d02f8918f95c606b18e3b5698448c59231e
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-07-13 10:45:59 -0400

    new script, new file copies

---
+++ UN/find_unscrs.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 12 18:19:21 2015
+
+@author: alex
+"""
+
+''' Read file, if it contains a certain string, rename it '''
+
+# testing finding lines
+for line in open("2014-N1469720.txt"):
+ if "United Nations" in line:
+   print line
+   
+# testing opening files in folder
+# this was a surprisingly quick process given the amount of lines to print!
+import glob
+for filename in glob.glob('*.txt'):
+    for line in open(filename):
+        if "United Nations" in line:
+            print line
+        
+# testing other commands
+# chapter VII missions
+list_of_ch7 = []
+import glob
+for filename in glob.glob('*.txt'):
+    for line in open(filename):
+        if "under Chapter VII" in line:
+            print filename
+            list_of_ch7.append(filename)
+            
+print list_of_ch7
+
+# embargoes
+embargoes = []
+for filename in glob.glob('*.txt'):
+    for line in open(filename):
+        if "States shall prevent" in line:
+            print filename
+            embargoes.append(filename)
+            
+print embargoes
+
+# all necessary means
+all_means = []
+for filename in glob.glob('*.txt'):
+    for line in open(filename):
+        if "use all necessary means" in line:
+            print filename
+            all_means.append(filename)
+            
+print all_means
+
+# observers
+observers = []
+for filename in glob.glob('*.txt'):
+    for line in open(filename):
+        if "United Nations observers" in line:
+            print filename
+            observers.append(filename)
+            
+print observers
+
+# other deployments
+deploy = []
+for filename in glob.glob('*.txt'):
+    for line in open(filename):
+        if "identify personnel, plan and make" in line:
+            print filename
+            deploy.append(filename)
+            
+print deploy
+            
+# define a single function for this
+def find_files(lists, expressions):
+    for filename in glob.glob('*.txt'):
+        for line in open(filename):
+            if expressions in line:
+                print filename
+                lists.append(filename)
+    print lists
+    
+# run function 
+# extension of mission
+mission_extend = []
+find_files(mission_extend, "extend the present mandate")
+find_files(mission_extend, "extend the mandate")
+find_files(mission_extend, "renew the mandate")
+
+len(mission_extend) # 502
+len(set(mission_extend)) # 480 - includes duplicates, should deal with this later
+
+# drawdown of mission
+drawdown = []
+find_files(drawdown, "drawdown")
+len(drawdown)
+find_files(drawdown, "gradual reduction of")
+len(drawdown)
+len(set(drawdown))
+
+# define a function to explore the text to validate logic
+def explore(strings):
+    for filename in glob.glob('*.txt'):
+        for line in open(filename):
+            if strings in line:
+                print line
+
+explore("States shall prevent")
+
+explore("economic activities")
+
+# sanctions
+sanctions = []
+find_files(sanctions, "economic activities carried on, after the date of adoption of this")
+   
+# define rules for renaming
+# define bin numbers
+soft = "1"
+prohib = "2"
+hard = "3"
+extend = "4"
+hard_neg = "5" # mission drawdown
+prohib_neg = "6" # sanctions cancellation
+soft_neg = "7" # observer handover
+
+# everything else will be read as "no action"
+
+# copied all UNSCRs in working dir to other dir in case this blows up!
+
+
+
+
+   
--- UN/nltk_unscrs.py
+++ UN/nltk_unscrs.py
@@ -18,7 +18,7 @@ cfd = nltk.ConditionalFreqDist(
     (target, fileid[:4]) # separates the years out of the filenames for the X axis
     for fileid in scrs.fileids()
     for w in scrs.words(fileid)
-    for target in ['genocide'] # also looked at "sad", "cost", "urge", "condemn"
+    for target in ['genocide', 'sad'] # also looked at "sad", "cost", "urge", "condemn"
     if w.lower().startswith(target))
 
 cfd.plot()
