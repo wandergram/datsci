dcc09fc8f47f6f20279ef9364ae6c762e17574fe
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-07-13 16:57:02 -0400

    updated code for file renaming

--- UN/find_unscrs.py
+++ UN/find_unscrs.py
@@ -89,6 +89,21 @@ mission_extend = []
 find_files(mission_extend, "extend the present mandate")
 find_files(mission_extend, "extend the mandate")
 find_files(mission_extend, "renew the mandate")
+find_files(mission_extend, "extend its mandate")
+find_files(mission_extend, "adjust the mandate")
+find_files(mission_extend, "Decides to extend U")
+find_files(mission_extend, "Decides to extend M")
+find_files(mission_extend, "Decides to extend until")
+find_files(mission_extend, "Decides to extend the current mandate")
+find_files(mission_extend, "Extends the stationing")
+find_files(mission_extend, "to renew the mandate")
+find_files(mission_extend, "Decides to renew for a period of")
+find_files(mission_extend, "Decides to extend the existing mandate")
+find_files(mission_extend, "Authorizes the expansion of M")
+find_files(mission_extend, "Approves the expansion of M")
+find_files(mission_extend, "Decides to extend")
+find_files(mission_extend, "Decides to renew the mandates")
+find_files(mission_extend, "increase the overall force levels")
 
 len(mission_extend) # 502
 len(set(mission_extend)) # 480 - includes duplicates, should deal with this later
@@ -98,6 +113,8 @@ drawdown = []
 find_files(drawdown, "drawdown")
 len(drawdown)
 find_files(drawdown, "gradual reduction of")
+find_files(drawdown, "Decides to withdraw U")
+find_files(drawdown, "liquidation")
 len(drawdown)
 len(set(drawdown))
 
@@ -146,18 +163,72 @@ def rename_files(list_name, var_name):
 
 # rename files referencing hard measures
 rename_files(list_of_ch7, hard)
+find_files(list_of_ch7, "Chapter VII of the Charter")
+rename_files(list_of_ch7, hard)
 
 # rename files referencing mission drawdown
 rename_files(drawdown, hard_neg)
 
 # rename files referencing embargoes
 rename_files(embargoes, prohib)
+find_files(embargoes, "the measures on arms")
+rename_files(embargoes, prohib)
 
 # explore travel ban-related resolutions
 explore("prevent the entry into or transit through their territories")  
-# create travel ban list
+
+# create travel ban list, rename files
 travel_ban = []
 find_files(travel_ban, "prevent the entry into or transit through their territories") 
-
-# rename files
 rename_files(travel_ban, prohib)
+
+# soft measures - police presence
+explore("civilian police")
+civilian_police = []
+find_files(civilian_police, "civilian police")
+rename_files(civilian_police, soft)
+
+# soft measures - military observers
+explore("military observers")
+military_observ = []
+find_files(military_observ, "military observers")
+rename_files(military_observ, soft)
+
+# suspension of measures - this doesn't quite work, find other text
+# explore("shall be suspended")
+# suspend_sanctions = []
+# find_files(suspend_sanctions, "shall be suspended")
+# rename_files(suspend_sanctions, prohib_neg)
+
+# what else is out there
+explore("Decides to")
+explore("Decides to terminate")
+
+# suspension of prohibitive measures - review this
+suspend_prohib = []
+find_files(suspend_prohib, "Decides to terminate the prohibitions")
+find_files(suspend_prohib, "Decides to terminate the remaining prohibitions")
+rename_files(suspend_prohib, prohib_neg)
+
+# explore("Decides to renew for a period of")
+
+explore("liquidation")
+explore("Approves the expansion of M")
+explore("Authorizes the expansion of M")
+explore("Panel of Experts")
+
+# soft measures - experts
+experts = []
+find_files(experts, "Panel of Experts")
+rename_files(experts, soft)
+
+# what other decisions has it made
+explore("1. Decides to")
+explore("increase the overall force levels")
+explore("advance team")
+
+# soft measures - advance teams (observers, setup)
+advance_team = []
+find_files(advance_team, "advance team")
+rename_files(advance_team, soft)
+
