2f3072fcfd6c7b35d893eadcf427deebd50c1abe
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-07-15 15:09:43 -0400

    a script to explore, rename, categorize, re-organize files

---
+++ UN/unscr_cat.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 16:54:50 2015
+
+@author: alex
+"""
+
+''' A clean version of the script to 1) explore the text of UN Security Council 
+resolutions (SCRs) (outputs), 2) find files of interest, 3) determine how to categorize them, and 
+4) rename the files by category in order to 5) create a categorized corpus.
+In the corpus, the filenames follow the format of YYYY-NDOCNUMB.txt, 
+e.g. 1994-N1234567.txt
+
+Working directory: unscr3/
+'''
+
+
+import os
+import glob
+
+# 1. Explore text: 
+# Here I define a function to explore the text of the documents to validate my logic.
+# This is a quick way to understand in what collocations phrases are typically used
+# and what kinds of topics they tend to reference. 
+def explore(strings):
+    for filename in glob.glob('*.txt'):
+        for line in open(filename):
+            if strings in line:
+                print line
+                print filename # becomes very useful later to see which kinds of files the text is found in
+                
+# print line.count(line) - return to this
+
+# For example, actions the SC authorizes:
+explore("authorizes")
+# Phrases used in relation to arms embargoes:
+explore("States shall prevent")
+# Phrases used in relation to economic sanctions:
+explore("economic activities")
+
+''' The format of most SCRs is as follows: the first part outlines general positions
+of the Council on certain things, using verbs in gerund form ("welcoming", "noting"),
+while the second part states decisions taken, with verbs in the present tense ("decides",
+"urges"). Most substantive actions are described in the first 1-3 points of the second part;
+typically, if a resolution authorizes the deployment or extension of a mission or some
+other kind of UN delegation/body, this will be stated first. 
+'''
+# Here I validate the logic described above.
+explore("Decides")
+# Here I make an educated guess that anytime "under Chapter VII" is used, it will
+# always be in the context of "acting under Chapter VII (...)" and always in reference
+# to a mission authorized to use force in situations of crisis.
+explore("under Chapter VII")
+
+# Further text exploration
+explore("use all necessary means") # authorizing military personnel to shoot in defense of civilians
+explore("drawdown") # drawing down missions
+explore("Decides to extend") # extending a mission's mandate
+explore("prevent the entry into or transit through their territories") # enacting travel bans 
+
+# 2. Find files of interest based on text data, append their filenames to a list.
+# Note: I can't create an empty list within the function, otherwise an existing list
+# to which I would like to append other filenames based on other phrases found within
+# will be overwritten every time.
+def find_files(lists, expressions):
+    for filename in glob.glob('*.txt'):
+        for line in open(filename):
+            if expressions in line:
+                print filename # included for reference, to give an idea of how many there are and from what years
+                lists.append(filename)
+    print lists
+    
+# 3. Determine how to categorize files:
+# Thinking also about how to structure the code that will distribute these files
+# by category or "bin" within the directory in order to later build the categorized
+# corpus.
+    
+# Thematic organization
+# Soft measures
+police = "police_" # civilian police component
+embargo = "emb_" # embargoes
+adv_team = "adv_team_" # advance teams
+observers = "mil_observ_" # military observers
+ban = "transit_ban_" # transit ban - sanctions
+experts = "experts_" # establishment of panels of experts
+fund = "fund_" # establishment of trust fund for operations
+office = "office_"
+
+# Chapter VII mission
+chapter7 = "ch7_" # deployment of missions with military personnel under Chapter VII
+
+# Mission extension
+extend = "extend_" # extending mission mandates
+
+# Drawdown, reversal of measures
+drawdown = "drawdown_" # mission drawdown, termination of previously imposed measures
+suspend = "suspend_" # suspend prohibitions
+
+# Everything else will be read as "no action taken". Currently this seems to be
+# about 280 documents out of the corpus (1,250 total).
+
+# 4. Rename the files by category, based on whether their filenames are in a given topical list:
+def rename_files(list_name, var_name):
+    for filename in os.listdir("."):
+        if filename in list_name:
+            os.rename(filename, var_name+filename)
+            print filename # same as above, included for reference
+
+
+''' Categorization '''
+
+# I. Mission Extension
+explore("Decides to extend")
+explore("extend the present mandate")
+explore("extend its mandate")
+explore("decides to extend")
+explore("Approves the continuation of")
+mission_extend = []
+find_files(mission_extend, "renew the mandate")
+find_files(mission_extend, "extend its mandate")
+find_files(mission_extend, "adjust the mandate")
+find_files(mission_extend, "Decides to extend")
+find_files(mission_extend, "Decides, in this context, to extend")
+find_files(mission_extend, "Extends the stationing")
+find_files(mission_extend, "Decides to renew for a period of")
+find_files(mission_extend, "Authorizes the expansion of M")
+find_files(mission_extend, "Approves the expansion of M")
+find_files(mission_extend, "decides to extend")
+find_files(mission_extend, "Decides to renew the mandates")
+find_files(mission_extend, "increase the overall force levels")
+find_files(mission_extend, "Approves the continuation of")
+
+rename_files(mission_extend, extend)
+
+# II. Mission Drawdown
+explore("drawdown")
+explore("terminate the mandate of")
+mission_drawdown = []
+find_files(mission_drawdown, "drawdown")
+find_files(mission_drawdown, "gradual reduction of")
+find_files(mission_drawdown, "Decides to withdraw U")
+find_files(mission_drawdown, "liquidation")
+find_files(mission_drawdown, "terminate the mandate of")
+
+
+explore("Decides to terminate the prohibitions")
+explore("Decides to terminate the remaining prohibitions")
+suspend_prohib = []
+find_files(suspend_prohib, "Decides to terminate the prohibitions")
+find_files(suspend_prohib, "Decides to terminate the remaining prohibitions")
+
+rename_files(mission_drawdown, drawdown)
+rename_files(suspend_prohib, suspend)
+
+# III. Deployment/Authorization to act under Chapter VII
+explore("under Chapter VII")
+ch7 = []
+find_files(ch7, "under Chapter VII")
+rename_files(ch7, chapter7)
+
+# IV. Soft Measures
+explore("civilian police")
+civ_police = []
+find_files(civ_police, "civilian police")
+rename_files(civ_police, police)
+
+explore("military observers")
+military_observers = []
+find_files(military_observers, "military observers")
+rename_files(military_observers, observers)
+
+transit_ban = []
+find_files(transit_ban, "prevent the entry into or transit through their territories") 
+rename_files(transit_ban, ban)
+find_files(transit_ban, "States shall prevent")
+rename_files(transit_ban, ban)
+# note that these are mostly Chapter VII measures, but including them alongside
+# full-scale missions may not be correct from a methodological perspective
+
+explore("the measures on arms")
+arms_embargo = []
+find_files(arms_embargo, "the measures on arms")
+rename_files(arms_embargo, embargo)
+
+explore("advance team")
+advance_team = []
+find_files(advance_team, "advance team")
+rename_files(advance_team, adv_team)
+
+explore("Panel of Experts")
+panel_experts = []
+find_files(panel_experts, "Panel of Experts")
+rename_files(panel_experts, experts)
+
+explore("trust fund")
+trust_fund = []
+find_files(trust_fund, "trust fund")
+rename_files(trust_fund, fund)
+
+sg_office = []
+find_files(sg_office, "Requests the Secretary-General to establish the")
+rename_files(sg_office, office)
+
+# Other data explorations
+explore("States shall prevent")
+explore("increase the overall force levels")
+explore("disarmament, demobilization and reintegration")
+explore("targeted sanctions")
+explore("Requests the Secretary-General to establish the")
+explore("use all necessary means")
+
+# 5. Separate files into folders by category
+# Define a function for this
+import shutil
+source = r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed"
+destination = r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/extensions"
+
+category_terms = ["police", "emb", "adv_team", "mil_observ", "transit_ban", "experts", "fund", "office", "ch7", "extend", "drawdown", "suspend"]
+
+def copy_files(destination, word):
+    if not os.path.exists(destination):
+        os.makedirs(destination)               
+    for f in os.listdir(source):
+        if word in f:
+            shutil.copy(os.path.join(source,f), destination)
+'''
+For reference:
+# Thematic organization
+# Soft measures
+police = "police_" # civilian police component
+embargo = "emb_" # embargoes
+adv_team = "adv_team_" # advance teams
+observers = "mil_observ_" # military observers
+ban = "transit_ban_" # transit ban - sanctions
+experts = "experts_" # establishment of panels of experts
+fund = "fund_" # establishment of trust fund for operations
+office = "office_"
+
+# Chapter VII mission
+chapter7 = "ch7_" # deployment of missions with military personnel under Chapter VII
+
+# Mission extension
+extend = "extend_" # extending mission mandates
+
+# Drawdown, reversal of measures
+drawdown = "drawdown_" # mission drawdown, termination of previously imposed measures
+suspend = "suspend_" # suspend prohibitions
+'''
+
+# mission extensions
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/extensions", "extend")
+
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/chapter_7_mission", "ch7")
+        
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/drawing_down", "drawdown")  
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/drawing_down", "suspend")  
+
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "police")  
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "emb")  
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "adv_team")  
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "mil_observ")  
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "transit_ban")  
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "experts")
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "fund")  
+copy_files(r"/Users/alex/Desktop/datsci/UN/corpus/unscr3_renamed/measures_soft", "office")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
