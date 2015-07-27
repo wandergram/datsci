ec3be8237ddb35d0687bc0e951eb5c6e3897c144
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-07-26 20:59:52 -0400

    updated NLTK code

--- UN/nltk_pvs.py
+++ UN/nltk_pvs.py
@@ -10,7 +10,7 @@ sys.setdefaultencoding("utf8")
 
 import nltk
 from nltk.corpus import PlaintextCorpusReader
-corpus_root = 'Desktop/datsci/UN/corpus/pv/'
+corpus_root = 'Desktop/datsci/UN/corpus/meeting_records/'
 pvs = PlaintextCorpusReader(corpus_root, '.*', encoding='utf8')
 pvs.fileids()
 
@@ -22,32 +22,13 @@ For Mac OSX, I used an app called TEConverter to batch convert to UTF-8.
 '''
 
 # plots conditional frequency distributions of words across years
-# authorization of missions
+
+# genocide, atrocities, massacres, cleansing
 cfd = nltk.ConditionalFreqDist(
     (target, fileid[:4]) # separates the years out of the filenames for the X axis
     for fileid in pvs.fileids()
     for w in pvs.words(fileid)
-    for target in ['authorize'] 
-    if w.lower().startswith(target))
-
-cfd.plot()
-
-# deployments
-cfd = nltk.ConditionalFreqDist(
-    (target, fileid[:4]) # separates the years out of the filenames for the X axis
-    for fileid in pvs.fileids()
-    for w in pvs.words(fileid)
-    for target in ['authorize', 'mission', 'deploy'] 
-    if w.lower().startswith(target))
-
-cfd.plot()
-
-# missions
-cfd = nltk.ConditionalFreqDist(
-    (target, fileid[:4]) # separates the years out of the filenames for the X axis
-    for fileid in pvs.fileids()
-    for w in pvs.words(fileid)
-    for target in ['mission'] 
+    for target in ['genocid', 'atrocit', 'massacre', 'cleansing'] 
     if w.lower().startswith(target))
 
 cfd.plot()
