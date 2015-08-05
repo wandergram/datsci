9f5d5cfbda6760fc588098e8b3ed5dab4a182172
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-08-05 14:58:42 -0400

    dataset compilation & classifier update

--- UN/compiling_dataset_with_text.py
+++ UN/compiling_dataset_with_text.py
@@ -40,4 +40,65 @@ full_data = pd.merge(recs, meetingframe)
          
 # drop 'id' column
 full_data.drop('id', axis=1, inplace=True)
-         
+
+# Write to new CSV file
+# full_data.to_csv('full_dataset.csv', index=False, sep = ',') # this gets really screwed up
+# how to write clean to CSV?
+
+''' PART 1 - Train Test Split, Naive Bayes, SKLEARN'''
+
+# Train test split
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(full_data.meeting_text, full_data.category, random_state=1)
+X_train.shape # 927
+X_test.shape # 309
+
+from sklearn.feature_extraction.text import CountVectorizer
+vect = CountVectorizer()
+train_dtm = vect.fit_transform(X_train)
+test_dtm = vect.transform(X_test)
+
+# Naive Bayes
+from sklearn.naive_bayes import MultinomialNB
+nb = MultinomialNB()
+nb.fit(train_dtm, y_train)
+
+y_pred_class = nb.predict(test_dtm)
+
+# Calculate accuracy
+from sklearn import metrics
+metrics.accuracy_score(y_test, y_pred_class) # 54%
+
+# Print confusion matrix
+metrics.confusion_matrix(y_test, y_pred_class)
+
+y_pred_prob = nb.predict_proba(test_dtm)[:, 1]
+print y_pred_prob # to check
+metrics.roc_auc_score(y_test, y_pred_prob) # AUC score 0.64
+
+# Plot ROC curve
+import matplotlib.pyplot as plt
+fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred_prob)
+plt.plot(fpr, tpr)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('false positive rate')
+plt.ylabel('true positive rate - sensitivity')
+
+# false positives
+X_test[y_test < y_pred_class]
+
+# false negatives
+X_test[y_test > y_pred_class]
+
+''' PART 2 - NTLK '''
+# assemble corpus
+# save corpus
+# load corpus
+# train classifier
+
+
+
+
+
+
