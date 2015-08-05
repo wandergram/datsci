3ab2cf4e8b2f8c94e7a2b3311dedc894e4239d40
Alex Kapitanskaya <a.kapitanskaya@gmail.com>
2015-08-05 09:22:18 -0400

    updated compilation code

--- UN/compiling_dataset_with_text.py
+++ UN/compiling_dataset_with_text.py
@@ -6,12 +6,27 @@ Created on Tue Aug  4 22:05:54 2015
 """
 
 import glob
-import sys
 
 # working dir: meeting_records_clean_final
+# read all files in directory into a list, where 1 file = 1 list element
 list_of_meetings = []
 for filename in glob.glob('*.txt'):
      with open(filename, 'r') as f:
-         meetinglist = f.read()
+         list_of_meetings.append(f.read())
+         f.close()
+         
+len(list_of_meetings) #1236 ^^ THIS WORKED! It's slow, but it worked!
+# still contains \n though - will ask about this
+
+# create Series from list 
+import pandas as pd
+meetingseries = pd.Series(list_of_meetings)
+
+# create DF from Series
+meetingframe = meetingseries.to_frame('meeting_text') # create df, name column "meeting_text"
+
+
+
+
          
          
